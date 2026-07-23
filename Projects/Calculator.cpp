00a689bfb25d9b447ed93d1501aba96528041739
Sotir <sotirusama13@gmail.com>
2026-07-23 16:24:08 +0300

    Calculator.cpp code change

--- Projects/Calculator.cpp
+++ Projects/Calculator.cpp
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <stdlib.h>
 
 #define F_CPU 16000000UL
 #include <util/delay.h>
@@ -12,10 +13,10 @@
 void updateLCD(void);
 void sendCommand(unsigned char command);
 void sendData(unsigned char character);
+void sendDataString(char* str);
 void sendInitCommand(unsigned char command);
 void LCDInit();
 
-#define NULL 0x00
 
 #define rMask 0b00111100
 #define r1 PORTC5
@@ -29,7 +30,7 @@ void LCDInit();
 #define c3 PORTD5
 #define c4 PORTD3
 
-unsigned char rowCol = NULL;                // register to hold current row and column the 4 High bits for 4 rows and the 4 low bits for the columns
+unsigned char rowCol = 0x00;                // register to hold current row and column the 4 High bits for 4 rows and the 4 low bits for the columns
 bool pressed = 0;
 
 unsigned char rowColDecoder_keypad (unsigned char rowCol);
@@ -38,25 +39,64 @@ void colScan_keypad();
 void rowEn_KeyPad (unsigned char r);
 void call_keypad();
 
+int dtoi (unsigned char s);					// digit to int
+void operate (unsigned char digit);
+void calculate();
+void printRes();
+
+int res = 0;
+int operands[] = {0,0};
+unsigned char op = 0x00;
+bool opSelect = 0, zeroDiv = 0;
+char buffer[12];
+char *zeroDivMsg = "Zero Division Error";
+bool idle = 0;
 
 int main(void)
 {
 	LCDDDR |= LCDMask;
-	DDRA |= 1 << enable | 1 << RS;                     //Set control lines as output (high)
+	DDRA |= 1 << enable | 1 << RS;
 	DDRC &= ~(rMask);
 	DDRD &= ~(cMask);
+	
+	LCDInit();
+	sendData('A');
 
 	while(1) {
 		
-		LCDInit();
-		
+		call_keypad();
+
 		unsigned char letter = rowColDecoder_keypad(rowCol);
-		if (!letter) continue;
-		switch (letter)
-		{
-		case '1':
-			
-			break;
+		
+		if (letter) {
+			if (letter == 'c') {
+				res = 0;
+				operands[0] = 0;
+				operands[1] = 0;
+				op = 0x00;
+				opSelect = 0;
+				zeroDiv = 0;
+				idle = 0;
+				sendCommand(0x01);
+				_delay_ms(60);
+				} else if (!idle && letter == '=') {
+				sendCommand(0x94);
+				sendData('=');
+				calculate();
+				if (zeroDiv) {
+					sendDataString(zeroDivMsg);
+				}
+				else {
+					itoa(res, buffer, 10);
+					sendDataString(buffer);
+				}
+				idle = 1;
+
+				} else if (!idle){
+				operate(letter);
+				sendData(letter);
+			}
+			_delay_ms(300);
 		}
 	}
 
@@ -73,7 +113,6 @@ void updateLCD() {
 }
 
 void sendCommand(unsigned char command) {
-
 	PORTA &= ~(1 << RS);     //Set RS low (write command)
 	LCDPort |= ((((command >> 4) & ~0x07) << 1) | ((command >> 4) & 0x07)) & LCDMask;             //Send 4 ms bits
 	updateLCD();
@@ -105,30 +144,30 @@ void sendInitCommand(unsigned char command) {
 }
 
 void LCDInit(){
-		_delay_ms(100);                              //Wait for LCD to boot
-		sendInitCommand(0x3);                        //Init function set 1
-		_delay_ms(100);
-		sendInitCommand(0x3);                        //Init function set 2
-		_delay_us(100);
-		sendInitCommand(0x3);                        //Init function set 3
-		_delay_us(100);
-		sendInitCommand(0x2);                        //Function set (set 4 bit mode)
-		_delay_us(100);
-		sendInitCommand(0x28);                       //Funcion set I=1, N=0, F=0
-		//sendInitCommand(0x8);
-		_delay_us(60);
-		sendInitCommand(0x8);                        //On/off control D=0, C=0, B=0
-		//sendInitCommand(0x8);
-		_delay_us(60);
-		sendCommand(0x01);                           //Clear display
-		//sendInitCommand(0x1);
-		_delay_ms(60);
-		sendCommand(0x06);                           //Entry mode set I/D=1, S=0
-		//sendInitCommand(0x6);
-		_delay_us(60);
-		sendCommand(0x0C);                           //On/off control D=1, C=0, B=0
-		//sendInitCommand(0xC);
-		_delay_us(60);
+	_delay_ms(100);                              //Wait for LCD to boot
+	sendInitCommand(0x3);                        //Init function set 1
+	_delay_ms(100);
+	sendInitCommand(0x3);                        //Init function set 2
+	_delay_us(100);
+	sendInitCommand(0x3);                        //Init function set 3
+	_delay_us(100);
+	sendInitCommand(0x2);                        //Function set (set 4 bit mode)
+	_delay_us(100);
+	sendInitCommand(0x28);                       //Funcion set I=1, N=0, F=0
+	//sendInitCommand(0x8);
+	_delay_us(60);
+	sendInitCommand(0x8);                        //On/off control D=0, C=0, B=0
+	//sendInitCommand(0x8);
+	_delay_us(60);
+	sendCommand(0x01);                           //Clear display
+	//sendInitCommand(0x1);
+	_delay_ms(60);
+	sendCommand(0x06);                           //Entry mode set I/D=1, S=0
+	//sendInitCommand(0x6);
+	_delay_us(60);
+	sendCommand(0x0C);                           //On/off control D=1, C=0, B=0
+	//sendInitCommand(0xC);
+	_delay_us(60);
 }
 
 void rowEn_KeyPad (unsigned char r){
@@ -140,15 +179,13 @@ void rowEn_KeyPad (unsigned char r){
 }
 
 void colRead_keyPad (unsigned char c){
-	unsigned char m = (c==c1)?0x08:(c==c2)?0x04:(c==c1)?0x02:(c==c2)?0x01:0x00;
-	if(!(PIND & (1<<c)))
-	if(!pressed){
-		rowCol |= (1<<m);
-		pressed=1;
-		rowColDecoder_keypad(rowCol);
+	// 3. Fixed the c3 and c4 typo
+	unsigned char m = (c==c1)?0x08:(c==c2)?0x04:(c==c3)?0x02:(c==c4)?0x01:0x00;
+	
+	if(!(PIND & (1<<c))){
+		// 4. Fixed the bitshift overflow (m is already the mask)
+		rowCol |= m;
 	}
-	else
-	pressed = 0;
 }
 
 void colScan_keypad(){
@@ -159,14 +196,17 @@ void colScan_keypad(){
 }
 
 void call_keypad(){
-	rowEn_KeyPad(r1);
-	rowEn_KeyPad(r2);
-	rowEn_KeyPad(r3);
-	rowEn_KeyPad(r4);
+	rowCol = 0; // Clear any previous state
+	
+	// 2. Abort scanning if a column press (lower 4 bits) is detected
+	rowEn_KeyPad(r1); if(rowCol & 0x0F) return;
+	rowEn_KeyPad(r2); if(rowCol & 0x0F) return;
+	rowEn_KeyPad(r3); if(rowCol & 0x0F) return;
+	rowEn_KeyPad(r4); if(rowCol & 0x0F) return;
 }
 
 unsigned char rowColDecoder_keypad (unsigned char rowCol){
-	unsigned char letter = NULL;
+	unsigned char letter = 0x00;
 	switch (rowCol)
 	{
 		case 0x88:
@@ -218,9 +258,48 @@ unsigned char rowColDecoder_keypad (unsigned char rowCol){
 		letter = '/';
 		break;
 		default:
-		letter = NULL;
+		letter = 0x00;
 		break;
 	}
 	
 	return letter;
+}
+
+int dtoi (unsigned char s){
+	return (int) s - 48;
+}
+
+void operate (unsigned char digit){
+	if (digit == '+' || digit == '-' || digit == '*' || digit == '/') {
+		opSelect = !opSelect;
+		op = digit;
+		return;
+	}
+	
+	int dig = dtoi(digit), operand = (!opSelect)?operands[0]:operands[1];
+	operand = operand * 10 + dig;
+	if (!opSelect) operands[0] = operand;
+	else operands[1] = operand;
+}
+
+void calculate(){
+	if (op == '+') res = operands[0] + operands[1];
+	else if (op == '-') res = operands[0] - operands[1];
+	else if (op == '*') res = operands[0] * operands[1];
+	else {
+		if (operands[1] == 0) {
+			zeroDiv = 1;
+			return;
+		}
+		res = operands[0] / operands[1];
+	}
+}
+
+void sendDataString(char* str) {
+	int i = 0;
+
+	while (str[i] != '\0') {
+		sendData(str[i]); // Send the current character to the LCD
+		i++;              // Move to the next character
+	}
 }
